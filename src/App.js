65184df63c0e1c68522a8a53b827b16713ed6475
Fuad Khan <fuadkhanantor1@gmail.com>
2022-08-24 00:39:32 +0600

    pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { Routes ,Route} from 'react-router-dom';
 import './App.css';
-
+import Banner from './Pages/Home/Banner/Banner';
+import Footer from './Pages/Home/Footer/Footer';
+import Header from './Pages/Home/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Services from './Pages/Home/Services/Services';
+import NotFound from './Pages/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/banner' element={<Banner></Banner>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Anisul Islam Courses</h2>
+        </div>
+    );
+};
+
+export default Home;
